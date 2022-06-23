fa8f1cfd05b0f69951c713be95a8250be2486548
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2022-06-23 19:52:26 -0300

    Add debugging and eslint configurations

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,9 @@
-import { json, Router } from 'express'
+// Aqui no import ele chama nesse formato entre cahves abaixo por que o  que está sendo chamado dentro dele
+// Seria a mesma coisa de um express.Router, ou seja, ele chama essas duas propriedades que estão dentro de App lá são passadas como express
+import { json, Router } from 'express';
 
+const routes = new Router();
 
-const routes = new Router()
+routes.get('/test', (req, res) => res.json({ ok: true }));
 
-
-routes.get('/test', (req, res) => {
-    return res.json({ ok: true })
-})
-
-
-export default routes
+export default routes;
