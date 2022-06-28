b236665cd842bab2be5459bb9a21405ae2fa47c8
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2022-06-28 02:34:27 -0300

    Starting development of routes and business rules

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,14 @@
+import User from '../models/User';
+
+
+
+class UserController {
+
+async store(req, res) {
+    return res.json({ok: true})
+}
+
+
+}
+
+export default new UserController()
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,10 @@
 // Aqui no import ele chama nesse formato entre cahves abaixo por que o  que está sendo chamado dentro dele
 // Seria a mesma coisa de um express.Router, ou seja, ele chama essas duas propriedades que estão dentro de App lá são passadas como express
 import { json, Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/test', async (req, res) => {
-    const user = await User.create({
-        name: 'Arthur',
-        email: 'arthur@gmail.com',
-        password_hash: '1234',
-    });
-   return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
