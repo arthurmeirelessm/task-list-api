6fca21d8c4d5b6a88b452529e3f8ca7232ab8765
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2022-06-28 03:32:15 -0300

    Finishing and testing if have same email in usercontroller (store)

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,14 +1,25 @@
 import User from '../models/User';
 
-
-
 class UserController {
 
-async store(req, res) {
-    return res.json({ok: true})
-}
+    async store(req, res) {
+        
+        const emailExists = req.body.email
+        const userIsSame = await User.findOne({
+            where: { email: emailExists }
+        });
 
+        if (userIsSame) {
+            return res.status(401).json({ Error: 'This user already exists' });
+        }
 
+        const { id, name, email } = await User.create(req.body);
+        return res.json({
+            id,
+            name,
+            email,
+        });
+    }
 }
 
 export default new UserController()
