a7b6d97331e98efdba693f1b0f91d5e984f9d8fd
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2022-06-30 19:33:21 -0300

    Starting creation logical of function for update in usercontro

--- src/app/middlewares/authMiddle.js
+++ src/app/middlewares/authMiddle.js
@@ -1,16 +1,22 @@
 import jwt from 'jsonwebtoken';
 import authContent from '../../config/auth';
-
-export default (req, res, next) => {
-const authHeader = req.headers.authorization;
-
-if (!authHeader) {
-    return res.status(401).json({ Error: 'Header authorization not exists.'})
-}
-
-const token = authHeader.split(' ');
+import { promisify } from 'util';
 
 
+export default async (req, res, next) => {
+    const authHeader = req.headers.authorization;
 
-return next();
+    if (!authHeader) {
+        return res.status(401).json({ Error: 'Header authorization not exists.' })
+    }
+
+    const [, token] = authHeader.split(' ');
+
+    try {
+        const decoded = await promisify(jwt.verify)(token, authContent.secret);
+        req.userId = decoded.id
+        return next()
+    } catch (err) {
+        return res.status(402).json({ Error: 'Invalid token' })
+    }
 }
