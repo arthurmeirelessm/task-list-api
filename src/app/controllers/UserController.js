f1014c0fbffdb0ece5dc82992b4e0060afca8643
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2022-07-01 23:37:46 -0300

    Finishing and testing yup validations

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -8,12 +8,8 @@ class UserController {
     async store(req, res) {
         const schema = Yup.object().shape({
             name: Yup.string().required(),
-            email: Yup.string()
-            .email()
-            .required(),
-            password: Yup.string()
-            .required()
-            .min(6),
+            email: Yup.string().email().required(),
+            password: Yup.string().required().min(6),
         });
 
         if (!(await schema.isValid(req.body))) {
@@ -37,6 +33,22 @@ class UserController {
     }
 
     async update(req, res) {
+        const schema = Yup.object().shape({
+            name: Yup.string(),
+            email: Yup.string().email(),
+            oldpassword: Yup.string().required().min(6),
+            password: Yup.string().min(6).when('oldpassword', (oldpassword, field) => {
+                oldpassword ? field.required() : field
+            }),
+            confirmpassword: Yup.string().when('password', (password, field) => {
+                password ? field.required().oneOf([Yup.ref('password')]) : field
+        }),
+        });
+
+        if(!(schema.isValid(req.body))) {
+            return res.status(400).json({Error: 'Invalid request' })
+        }
+
         const { email, oldpassword } = req.body;
         const user = await User.findByPk(req.userId);
 
@@ -51,7 +63,7 @@ class UserController {
             return res.status(401).json({ Error: 'Invalid password' });
         }
 
-        const {id, name} = await user.update(req.body);
+        const { id, name } = await user.update(req.body);
 
         return res.json({
             id,
