6032111203c2c604624ed6fbe7abf48de342e3b9
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2022-07-01 23:57:56 -0300

    Create tasks migration in postgre

--- src/database/migrations/20220702024213-create-tasks.js
+++ src/database/migrations/20220702024213-create-tasks.js
@@ -1,21 +1,43 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('tasks', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      task: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      check: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        referecnes: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL', 
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+
+  down: queryInterface => {
+    return queryInterface.dropTable('tasks');
+  },
 };
